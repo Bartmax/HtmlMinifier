5160b666d245acefa55d595519107b6b86563c79
Bart Calixto <bart@bartmax.com>
2015-04-25 17:15:42 -0300

    initial commit

---
+++ src/HtmlMinifier.MiddleWare/HtmlMinifierMiddleWare.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using System.IO;
+using System.Threading.Tasks;
+using ZetaHtmlCompressor;
+
+namespace HtmlMinifier.MiddleWare
+{
+    public class HtmlMinifierMiddleWare
+    {
+        RequestDelegate _next;
+
+        public HtmlMinifierMiddleWare(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                var bodyStream = context.Response.Body;
+                context.Response.Body = memoryStream;
+
+                await _next(context);
+
+                var isHtml = context.Response.ContentType?.ToLower().Contains("text/html");
+                if (context.Response.StatusCode == 200 && isHtml.GetValueOrDefault())
+                {
+                    {
+                        memoryStream.Seek(0, SeekOrigin.Begin);
+                        using (var streamReader = new StreamReader(memoryStream))
+                        {
+                            string body = await streamReader.ReadToEndAsync();
+                            body = MinifyHtml(body);
+                            using (var minBodyStream = new MemoryStream())
+                            using (var streamWriter = new StreamWriter(minBodyStream))
+                            {
+                                streamWriter.Write(body);
+                                streamWriter.Flush();
+                                minBodyStream.Seek(0, SeekOrigin.Begin);
+                                await minBodyStream.CopyToAsync(bodyStream);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private static string MinifyHtml(string responseBody)
+        {
+            var compressor = new HtmlContentCompressor();
+            return compressor.Compress(responseBody);
+        }
+    }
+}
---
+++ src/HtmlMinifier.MiddleWare/StaticFileExtensions.cs
@@ -0,0 +1,15 @@
+﻿using HtmlMinifier.MiddleWare;
+using System;
+
+namespace Microsoft.AspNet.Builder
+{
+    public static class StaticFileExtensions
+    {
+        public static IApplicationBuilder UseHtmlMinifier(this IApplicationBuilder builder)
+        {
+            if (builder == null) throw new ArgumentNullException(nameof(builder));
+            builder.UseMiddleware<HtmlMinifierMiddleWare>();
+            return builder;
+        }
+    }
+}
